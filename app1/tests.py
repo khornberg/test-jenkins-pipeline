d966e3305713ea4342d3f0dd1a970965f8d56c20
Kyle Hornberg <kyle.hornberg@gmail.com>
2016-07-20 14:33:39 -0400

    django project with two apps and slow tests

---
+++ app1/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+import time
+
+
+class App1Tests(TestCase):
+
+    def test_app1_one(self):
+        self.assertEqual(1, 1)
+
+    def test_app1_long(self):
+        time.sleep(3)
+        self.assertEqual(True, True)
---
+++ app2/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+import time
+
+
+class App2Tests(TestCase):
+
+    def test_app2_one(self):
+        self.assertEqual(2, 2)
+
+    def test_app2_long(self):
+        time.sleep(3)
+        self.assertEqual(True, True)
